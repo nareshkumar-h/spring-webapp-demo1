b102d7e3d3ba5a07a57051ee3be3a56a31b8db34
Naresh Kumar H <nareshkumarh@live.com>
2019-07-10 16:21:22 -0400

    Updated

--- src/main/java/com/naresh/bankingapp/config/DataSourceConfig.java
+++ src/main/java/com/naresh/bankingapp/config/DataSourceConfig.java
@@ -6,8 +6,10 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
@@ -16,18 +18,32 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @EnableTransactionManagement
 public class DataSourceConfig {
 
+	@Value("${dbUsername}")
+	private String username;
+	
+	@Value("${dbPassword}")
+	private String password;
+	
+	@Value("${dbUrl}")
+	private String url;
+	
+	@Value("${driverClassName}")
+	private String driverClassName;
+	
+	
 	@Bean
 	@Scope("singleton") //"prototype", "request", "application",session
 	public DataSource dataSource() {
 		System.out.println("Creating dataSource");
 		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		ds.setUrl("jdbc:oracle:thin:@trainingdb.cmqwe8vrjnur.us-east-1.rds.amazonaws.com:1521:orcl");
-		ds.setUsername("naresh");
-		ds.setPassword("password");
+		ds.setDriverClassName(driverClassName);
+		ds.setUrl(url);
+		ds.setUsername(username);
+		ds.setPassword(password);
 		return ds;
 	}
 
